4b0ce9ebf054d33841b86fe2896c6bd23d15b2d8
ma-04 <120931948+ma-04@users.noreply.github.com>
2025-02-19 11:42:45 +0600

    add 8.4

--- build.sh
+++ build.sh
@@ -7,3 +7,4 @@ docker build --platform linux/amd64,linux/arm64 --build-arg PHP_VERSION=8.0 -t f
 docker build --platform linux/amd64,linux/arm64 --build-arg PHP_VERSION=8.1 -t flywp/php:8.1 -f php/Dockerfile php --push
 docker build --platform linux/amd64,linux/arm64 --build-arg PHP_VERSION=8.2 -t flywp/php:8.2 -f php/Dockerfile php --push
 docker build --platform linux/amd64,linux/arm64 --build-arg PHP_VERSION=8.3 -t flywp/php:8.3 -f php/Dockerfile php --push
+docker build --platform linux/amd64,linux/arm64 --build-arg PHP_VERSION=8.4 -t flywp/php:8.4 -f php/Dockerfile php --push
